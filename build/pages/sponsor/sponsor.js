121878854988ad5fa54824eaa7138aae11191ee7
frying <6719537@qq.com>
2026-08-15 17:56:14 +0800

    chore: sign 璇佷功/绉侀挜銆乨ist RPK 鎴愬搧銆乥uild 涓棿浜х墿绾冲叆鐗堟湰绠＄悊锛坣ode_modules 浠嶅拷鐣ワ級

---
+++ build/pages/sponsor/sponsor.js
@@ -0,0 +1,245 @@
+export default function(global, globalThis, window, $app_exports$, $app_evaluate$) {
+    var org_app_require = $app_require$;
+    (function(global, globalThis, window, $app_exports$, $app_evaluate$) {
+        var setTimeout = global.setTimeout;
+        var setInterval = global.setInterval;
+        var clearTimeout = global.clearTimeout;
+        var clearInterval = global.clearInterval;
+        var $app_require$1 = global.$app_require$ || org_app_require;
+        var createPageHandler = function() {
+            return (()=>{
+                var __webpack_modules__ = {};
+                var __webpack_module_cache__ = {};
+                function __webpack_require__(moduleId) {
+                    var cachedModule = __webpack_module_cache__[moduleId];
+                    if (void 0 !== cachedModule) return cachedModule.exports;
+                    var module = __webpack_module_cache__[moduleId] = {
+                        exports: {}
+                    };
+                    __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+                    return module.exports;
+                }
+                (()=>{
+                    __webpack_require__.rv = ()=>"1.7.12";
+                })();
+                (()=>{
+                    __webpack_require__.ruid = "bundler=rspack@1.7.12";
+                })();
+                var $app_style$ = [
+                    [
+                        [
+                            [
+                                0,
+                                "page"
+                            ]
+                        ],
+                        {
+                            width: "100%",
+                            height: "100%",
+                            paddingTop: "24px",
+                            backgroundColor: "#020813",
+                            flexDirection: "column",
+                            alignItems: "center"
+                        }
+                    ],
+                    [
+                        [
+                            [
+                                0,
+                                "back-button"
+                            ]
+                        ],
+                        {
+                            width: "78px",
+                            height: "54px",
+                            borderRadius: "22px",
+                            backgroundColor: "#0b65ea",
+                            justifyContent: "center",
+                            alignItems: "center"
+                        }
+                    ],
+                    [
+                        [
+                            [
+                                0,
+                                "back-text"
+                            ]
+                        ],
+                        {
+                            width: "70px",
+                            height: "30px",
+                            color: "#ffffff",
+                            fontSize: "22px",
+                            fontWeight: 800,
+                            textAlign: "center"
+                        }
+                    ],
+                    [
+                        [
+                            [
+                                0,
+                                "sponsor-code"
+                            ]
+                        ],
+                        {
+                            width: "188px",
+                            height: "188px",
+                            marginTop: "68px"
+                        }
+                    ],
+                    [
+                        [
+                            [
+                                0,
+                                "sponsor-caption"
+                            ]
+                        ],
+                        {
+                            marginTop: "16px",
+                            color: "#ffffff",
+                            fontSize: "22px",
+                            fontWeight: 800,
+                            textAlign: "center"
+                        }
+                    ]
+                ];
+                var $app_script$ = function __scriptModule__(module, exports, $app_require$1) {
+                    "use strict";
+                    Object.defineProperty(exports, "__esModule", {
+                        value: true
+                    });
+                    exports.default = void 0;
+                    var _system = _interopRequireDefault($app_require$1("@app-module/system.router"));
+                    function _interopRequireDefault(e) {
+                        return e && e.__esModule ? e : {
+                            default: e
+                        };
+                    }
+                    var _default = exports.default = {
+                        private: {
+                            touchStartX: -1,
+                            touchStartY: -1
+                        },
+                        goBack () {
+                            _system.default.back();
+                        },
+                        onTouchStart (evt) {
+                            const point = this.getTouchPoint(evt);
+                            if (point) {
+                                this.touchStartX = point.clientX;
+                                this.touchStartY = point.clientY;
+                            }
+                        },
+                        onTouchEnd (evt) {
+                            const point = this.getTouchPoint(evt);
+                            if (!point || this.touchStartX < 0) return;
+                            const endX = point.clientX;
+                            const endY = point.clientY;
+                            const sw = this.$app.$def.data.screenWidth || 212;
+                            const startsInLeftQuarter = this.touchStartX <= 0.25 * sw;
+                            const endsAtRightQuarter = endX >= 0.75 * sw;
+                            const mostlyHorizontal = Math.abs(endY - this.touchStartY) <= 120;
+                            if (startsInLeftQuarter && endsAtRightQuarter && mostlyHorizontal) _system.default.back();
+                            this.touchStartX = -1;
+                            this.touchStartY = -1;
+                        },
+                        getTouchPoint (evt) {
+                            if (evt && evt.changedTouches && evt.changedTouches.length > 0) return evt.changedTouches[0];
+                            if (evt && evt.touches && evt.touches.length > 0) return evt.touches[0];
+                            return null;
+                        }
+                    };
+                    const moduleOwn = exports.default || module.exports;
+                    const accessors = [
+                        'public',
+                        'protected',
+                        'private'
+                    ];
+                    if (moduleOwn.data && accessors.some(function(acc) {
+                        return moduleOwn[acc];
+                    })) throw new Error('页面VM对象中的属性data不可与"' + accessors.join(',') + '"同时存在，请使用private替换data名称');
+                    if (!moduleOwn.data) {
+                        moduleOwn.data = {};
+                        moduleOwn._descriptor = {};
+                        accessors.forEach(function(acc) {
+                            const accType = typeof moduleOwn[acc];
+                            if ('object' === accType) {
+                                moduleOwn.data = Object.assign(moduleOwn.data, moduleOwn[acc]);
+                                for(const name in moduleOwn[acc])moduleOwn._descriptor[name] = {
+                                    access: acc
+                                };
+                            } else if ('function' === accType) console.warn('页面VM对象中的属性' + acc + '的值不能是函数，请使用对象');
+                        });
+                    }
+                };
+                var $app_template$ = function(vm) {
+                    const _vm_ = vm || this;
+                    return aiot.__ce__("div", {
+                        __vm__: _vm_,
+                        __opts__: {
+                            classList: [
+                                "page"
+                            ],
+                            events: {
+                                touchstart: function(evt) {
+                                    return _vm_.onTouchStart(evt);
+                                },
+                                touchend: function(evt) {
+                                    return _vm_.onTouchEnd(evt);
+                                }
+                            }
+                        }
+                    }, [
+                        aiot.__ce__("div", {
+                            __vm__: _vm_,
+                            __opts__: {
+                                classList: [
+                                    "back-button"
+                                ],
+                                events: {
+                                    click: function(evt) {
+                                        return _vm_.goBack(evt);
+                                    }
+                                }
+                            }
+                        }, [
+                            aiot.__ce__("text", {
+                                __vm__: _vm_,
+                                __opts__: {
+                                    classList: [
+                                        "back-text"
+                                    ],
+                                    value: "返回"
+                                }
+                            }, [])
+                        ]),
+                        aiot.__ce__("image", {
+                            __vm__: _vm_,
+                            __opts__: {
+                                classList: [
+                                    "sponsor-code"
+                                ],
+                                src: "/common/sponsor-code.png"
+                            }
+                        }, []),
+                        aiot.__ce__("text", {
+                            __vm__: _vm_,
+                            __opts__: {
+                                classList: [
+                                    "sponsor-caption"
+                                ],
+                                value: "求喂饭"
+                            }
+                        }, [])
+                    ]);
+                };
+                $app_exports$['entry'] = function($app_exports$) {
+                    $app_script$({}, $app_exports$, $app_require$1);
+                    $app_exports$.default.template = $app_template$;
+                    $app_exports$.default.style = $app_style$;
+                };
+            })();
+        };
+        return createPageHandler();
+    })(global, globalThis, window, $app_exports$, $app_evaluate$);
+}
