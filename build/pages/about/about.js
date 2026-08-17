3960adf4d2a9208436bbc751cdde6d492d367183
frying <6719537@qq.com>
2026-08-17 17:53:46 +0800

    feat: update about page card width

--- build/pages/about/about.js
+++ build/pages/about/about.js
@@ -129,7 +129,7 @@ export default function(global, globalThis, window, $app_exports$, $app_evaluate
                             ]
                         ],
                         {
-                            width: "432px",
+                            width: "402px",
                             height: "240px",
                             paddingLeft: "18px",
                             paddingTop: "24px",
